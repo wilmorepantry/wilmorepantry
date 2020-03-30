b3ff9f638fb57285669d924ef281f4401d8644a2
laura <lauras_here@pm.me>
2020-03-30 15:51:19 -0700

    Update styles for mobile:

--- _includes/components/button.js
+++ _includes/components/button.js
@@ -3,14 +3,14 @@ module.exports = ({ href, text, color = "blue-400" } = {}) => {
     return `
 <a
     href="${href}"
-    class="block w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
+    class="block w-full md:w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
     >${text}</a
 >
 		`;
   }
   return `
 <button
-    class="block w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
+    class="block w-full md:w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
     >${text}</button
 >
   `;
