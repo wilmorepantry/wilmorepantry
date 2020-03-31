5daa6d1b12e289adbf7f533892e45ce47a20d1d0
laura <lauras_here@pm.me>
2020-03-30 20:28:03 -0700

    Update styles

--- _includes/components/button.js
+++ _includes/components/button.js
@@ -3,14 +3,14 @@ module.exports = ({ href, text, color = "blue-400" } = {}) => {
     return `
 <a
     href="${href}"
-    class="block w-full md:w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
+    class="block w-full md:w-1/2 p-2 my-4 mx-auto border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold text-xl"
     >${text}</a
 >
 		`;
   }
   return `
 <button
-    class="block w-full md:w-1/2 p-2 my-4 border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold"
+    class="block w-full md:w-1/2 p-2 my-4 mx-auto border-4 border-${color} font-sans text-center hover:bg-${color} font-semibold text-xl"
     >${text}</button
 >
   `;
---
+++ pages/donate.md
@@ -0,0 +1,11 @@
+---
+section: donate
+title: Donate
+permalink: /#donate
+eleventyNavigation:
+  key: Donate
+  order: 15
+callToAction:
+  content: ""
+  header: ""
+---
